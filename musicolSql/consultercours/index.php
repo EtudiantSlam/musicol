58c8d5dbb3a10d11bbd6f5324e4ae22f2a2e588c
Guy VERGHOTE <gverghote@stremi.fr>
2025-04-29 14:40:08 +0200

    Dernière sauvegarde le 29/04/2025 à 12:40

--- musicolSql/consultercours/index.php
+++ musicolSql/consultercours/index.php
@@ -5,15 +5,12 @@ require $_SERVER['DOCUMENT_ROOT'] .  "/include/autoload.php";
 
 // chargement de l'interface
 $titre = "Consulter les cours d'une journée";
-// Instancier la classe Musicol
+$musicol = new Musicol(new Select());
+$data = json_encode($musicol->getLesJours());
 
-// Récupérer les jours dans le format json
-
-
-// Transmisssion des données à l'interface
 $head = <<<EOD
 <script>
-   
+    let data = $data;
 </script>
 EOD;
 
